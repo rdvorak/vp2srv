ba4aa66072e213635c932081b5b8748df8a457f7
U-OK\dvorbki <dvorbki@PCHDA0BL.ok.aero>
2016-12-29 11:56:24 +0100

    another commit

--- main.go
+++ main.go
@@ -5,15 +5,29 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/boltdb/bolt"
 	"gopkg.in/gin-gonic/gin.v1"
 )
 
+//Archive - message with archive data
+type Archive struct {
+	Start     int64
+	Interval  float64
+	HiOutTemp []float64
+	LoOutTemp []float64
+	Rain      []float64
+	Bar       []float64
+	HiWSpeed  []float64
+}
 type vanprod struct {
 	db      *bolt.DB
 	buckets []string
 	bucket  map[string]*bolt.Bucket
+	archive Archive
 }
 
 func (vp *vanprod) createBuckets() {
@@ -32,7 +46,48 @@ func (vp *vanprod) createBuckets() {
 		log.Fatal(err)
 	}
 }
-func (vp *vanprod) saveArchiveData(c *gin.Context) {
+func (vp *vanprod) getArchive(c *gin.Context) {
+	interval := time.Minute * 30
+	format := "2006-01-02T15:04:05Z"
+	iter := time.Now().Add(-3 * 24 * time.Hour).Truncate(interval)
+	vp.archive = Archive{
+		Start:    iter.Unix(),
+		Interval: interval.Seconds() * 1000,
+	}
+	vp.db.View(func(tx *bolt.Tx) error {
+		for ; time.Now().After(iter); iter = iter.Add(interval) {
+			// Assume our events bucket exists and has RFC3339 encoded time keys.
+			for _, b := range []string{"HI_OUTTEMP", "LO_OUTTEMP", "RAIN", "BAR", "HI_WSPEED"} {
+				v := tx.Bucket([]byte(b)).Get([]byte(iter.Format(format)))
+				s, err := strconv.ParseFloat(string(v), 64)
+				if v != nil && err != nil {
+					log.Println(err)
+				}
+				if v == nil {
+					s = nil
+				}
+				switch b {
+				case "HI_OUTTEMP":
+					vp.archive.HiOutTemp = append(vp.archive.HiOutTemp, s)
+				case "LO_OUTTEMP":
+					vp.archive.LoOutTemp = append(vp.archive.LoOutTemp, s)
+				case "RAIN":
+					vp.archive.Rain = append(vp.archive.Rain, s)
+				case "BAR":
+					vp.archive.Bar = append(vp.archive.Bar, s)
+				case "HI_WSPEED":
+					vp.archive.HiWSpeed = append(vp.archive.HiWSpeed, s)
+				}
+
+			}
+		}
+
+		return nil
+	})
+	c.JSON(http.StatusOK, vp.archive)
+
+}
+func (vp *vanprod) postArchive(c *gin.Context) {
 	defer c.Request.Body.Close()
 	//fmt.Println(ioutil.ReadAll(c.Request.Body))
 	csvr := csv.NewReader(c.Request.Body)
@@ -63,6 +118,10 @@ func (vp *vanprod) saveArchiveData(c *gin.Context) {
 }
 func main() {
 	router := gin.Default()
+	router.LoadHTMLGlob("tmpl/*")
+	router.GET("/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
 	var vp vanprod
 	db, err := bolt.Open("vp2.db", 0600, nil)
 	if err != nil {
@@ -77,6 +136,7 @@ func main() {
 		"pozdechov": "vp2",
 	}))
 
-	admin.POST("/submit/archive", vp.saveArchiveData)
+	router.GET("/archive", vp.getArchive)
+	admin.POST("/submit/archive", vp.postArchive)
 	router.Run(":8080")
 }
